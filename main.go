ec211b7c5c4b2abd51a11c96698d6195c52a00f4
tnaums <tanaumann@outlook.com>
2026-03-28 00:06:25 -0500

    Starting over, end chapter 1

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const inputFilePath = "messages.txt"
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer f.Close()
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Printf("%s\n", line)
+	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8, 8)
+			n, err := f.Read(b)
+			currentLineContents += string(b[:n])
+			parts := strings.SplitN(currentLineContents, "\n", 2)
+			if len(parts) > 1 {
+				lines <- parts[0]
+				currentLineContents = parts[1]
+			}
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+		}
+	}()
+	return lines
+}
