0b0644eb7fd0e6d7dca62ac206a5b9c428d8989b
tnaums <tanaumann@outlook.com>
2026-03-29 22:51:55 -0500

    ch4l3 starting

--- internal/request/request.go
+++ internal/request/request.go
@@ -6,8 +6,16 @@ import (
 	"io"
 )
 
+type RequestState int
+
+const (
+	initialized RequestState = iota
+	done
+)
+
 type Request struct {
 	RequestLine RequestLine
+	RequestState RequestState
 }
 
 type RequestLine struct {
@@ -16,49 +24,58 @@ type RequestLine struct {
 	Method        string
 }
 
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	b, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, err
 	}
-	requestLine, err := parseRequestLine(b)
+	requestLine, n, err := parseRequestLine(b)
 	if err != nil {
 		return nil, err
 	}
+
+	if n == 0 {
+		return nil, nil
+	}
 	return &Request{
-		RequestLine: requestLine,
+		RequestLine: *requestLine,
 	}, nil
 }
 
-func parseRequestLine(b []byte) (RequestLine, error) {
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
 	lines := bytes.Split(b, []byte("\r\n"))
 	parts := bytes.Split(lines[0], []byte(" "))
-
+	n := len(lines[0]) + 2
+	
 	// request line must have three parts
 	if len(parts) != 3 {
-		return RequestLine{}, fmt.Errorf("Requestline did not contain three parts")
+		return nil, 0, fmt.Errorf("Requestline did not contain three parts")
 	}
 	
 	// Method can only be uppercase letters
 	if checkASCII(parts[0]) == false {
-		return RequestLine{}, fmt.Errorf("Method contains invalid character")
+		return nil, 0, fmt.Errorf("Method contains invalid character")
 	}
 
 	// only HTTP 1.1 is supported
 	version := bytes.TrimPrefix(parts[2], []byte("HTTP/"))
-	if string(version) != "1.1" {
-		return RequestLine{}, fmt.Errorf("Only HTTP/1.1 is supported.")
+	if len(version) == len(parts[2]) {
+		return nil, 0, fmt.Errorf("malformed HTTP version: %s", parts[2])
 	}
-	return RequestLine{
+	if string(version) != "1.1" {
+		return nil, 0, fmt.Errorf("Only HTTP/1.1 is supported: %s", parts[2])
+	}
+	return &RequestLine{
 		HttpVersion: string(version),
 		RequestTarget: string(parts[1]),
 		Method: string(parts[0]),
-	}, nil
+	}, n, nil
 }
 
 func checkASCII(b []byte) bool {
 	for _, r := range b {
-		if r > 90 || r < 65 {
+		if r > 'Z' || r < 'A' {
 			return false
 		}
 	}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -8,9 +9,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+// Read reads up to len(p) or numBytesPerRead bytes from the string per call
+// its useful for simulating reading a variable number of bytes per chunk from a network connection
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	endIndex := cr.pos + cr.numBytesPerRead
+	if endIndex > len(cr.data) {
+		endIndex = len(cr.data)
+	}
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
+}
+
 func TestRequestLineParse(t *testing.T) {
 	// Test: Good GET Request line
-	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader := &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err := RequestFromReader(reader)
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
@@ -18,7 +45,11 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: Good GET Request line with path
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader = &chunkReader{
+		data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
@@ -26,11 +57,19 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: Invalid number of parts in request line
-	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader = &chunkReader{
+		data:            "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	_, err = RequestFromReader(reader)
 	require.Error(t, err)
 
 	// Test: Good POST request with path
-	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader = &chunkReader{
+		data:            "POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}	
+	r, err = RequestFromReader(reader)
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "POST", r.RequestLine.Method)
@@ -38,10 +77,18 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: out of order request
+	reader = &chunkReader{
+		data:            "/coffee GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}	
 	_, err = RequestFromReader(strings.NewReader("/coffee GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
 
 	// Test: Invalid version in request line
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/2.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader = &chunkReader{
+		data:            "GET /coffee HTTP/2.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}	
+	r, err = RequestFromReader(reader)
 	require.Error(t, err)
 }
