6cbbf2d5547a7a19b0f75df45d60a76c601959dd
tnaums <tanaumann@outlook.com>
2026-04-01 19:14:57 -0500

    second time, ch5l2 constraints, done

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -15,28 +16,52 @@ func NewHeaders() Headers {
 func (h Headers) Parse(data []byte) (int, bool, error) {
 	n := bytes.Index(data, []byte("\r\n"))
 
-	// not enough data; \r\n not found
 	if n == -1 {
 		return 0, false, nil
 	}
-	// \r\n found at start of data; done parsing headers
+
 	if n == 0 {
 		return 2, true, nil
 	}
-	
+
 	parts := bytes.SplitN(data[:n], []byte(":"), 2)
-	key := string(parts[0])
+	key := strings.ToLower(string(parts[0]))
 	if key != strings.TrimRight(key, " ") {
 		return 0, false, fmt.Errorf("invalid header name: %s", key)
 	}
 
 	value := bytes.TrimSpace(parts[1])
 	key = strings.TrimSpace(key)
-	
+	if !validTokens([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid header token found: %s", key)
+	}
 	h.Set(key, string(value))
 	return n + 2, false, nil
 }
 
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
 func (h Headers) Set(key, value string) {
 	h[key] = value
+}
+
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
+func validTokens(data []byte) bool {
+	for _, c := range data {
+		if !isTokenChar(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isTokenChar(c byte) bool {
+	if c >= 'A' && c <= 'Z' ||
+		c >= 'a' && c <= 'z' ||
+		c >= 0 && c <= 9 {
+		return true
+	}
+
+	return slices.Contains(tokenChars, c)
 }
