4052204b8c81e5d797f0f8d9fa17933f45e7ece5
tnaums <tanaumann@outlook.com>
2026-04-02 08:25:37 -0500

    ch7l1 Server, Serve function done

---
+++ internal/server/server.go
@@ -0,0 +1,24 @@
+package server
+
+type Server struct {
+	closed   atomic.Bool
+	listener net.Listener
+}
+
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	s := &Server{
+		listener: listener,
+	}
+	go s.listen()
+	return s, nil
+}
+
+func (s *Server) Close() error {}
+
+func (s *Server) listen() {}
+
+func (s *Server) handle(conn net.Conn) {}
