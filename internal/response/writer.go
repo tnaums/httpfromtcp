8c809aa9c9574348b4b8d4adbd0a2d0cdd7d72dd
tnaums <tanaumann@outlook.com>
2026-04-06 12:44:01 -0500

    second time, ch8l1 chunked encoding, starting

--- internal/response/writer.go
+++ internal/response/writer.go
@@ -27,6 +27,44 @@ func NewWriter(w io.Writer) *Writer {
 	}
 }
 
+func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
+	if w.writerState != writerStateBody {
+		return 0, fmt.Errorf("cannot write body in state %d", w.writerState)
+	}
+	chunkSize := len(p)
+
+	nTotal := 0
+	n, err := fmt.Fprintf(w.writer, "%x\r\n", chunkSize)
+	if err != nil {
+		return nTotal, err
+	}
+	nTotal += n
+
+	n, err = w.writer.Write(p)
+	if err != nil {
+		return nTotal, err
+	}
+	nTotal += n
+
+	n, err = w.writer.Write([]byte("\r\n"))
+	if err != nil {
+		return nTotal, err
+	}
+	nTotal += n
+	return nTotal, nil
+}
+
+func (w *Writer) WriteChunkedBodyDone() (int, error) {
+	if w.writerState != writerStateBody {
+		return 0, fmt.Errorf("cannot write body in state %d", w.writerState)
+	}
+	n, err := w.writer.Write([]byte("0\r\n\r\n"))
+	if err != nil {
+		return n, err
+	}
+	return n, nil
+}
+
 func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
 	if w.writerState != writerStateStatusLine {
 		return fmt.Errorf("cannot write status line in state %d", w.writerState)
